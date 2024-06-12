d15737daa70fb0fc43b4ee3487f0cb06f9493011
Matheus Ricardo <matheusricardo164@gmail.com>
2024-06-12 18:00:15 -0400

    Main

---
+++ script.js
@@ -0,0 +1,7 @@
+function aperta() {
+    const UnHideIt = document.querySelector('#toggle-hidden');
+    const UnHideItImage = document.querySelector('#image-hidden');
+
+    UnHideIt.classList.toggle('hidden');
+    UnHideItImage.classList.toggle('hidden');
+}
